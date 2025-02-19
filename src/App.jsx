af57c992cacc919a4cc8dba60a328b8a40d848b4
DevProdipraj <devprodipraj@gmail.com>
2025-02-19 15:17:12 +0600

    add hero area

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Component/Navbar'
+import HeroArea from './Component/HeroArea'
 
 const App = () => {
   return (
     <div>
     <Navbar />
+    <HeroArea />
     </div>
   )
 }
---
+++ src/Component/HeroArea.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroArea = () => {
+  return (
+    <div className=' bg-gray-400 '>
+    <div className='container  flex items-center justify-center flex-col  h-[90vh]'>
+      <h1 className='text-center text-green-700 text-6xl my-4 '>Hero area</h1>
+      <p className='w-4xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore soluta iusto reprehenderit perspiciatis quo obcaecati explicabo molestias, asperiores animi eos. Asperiores ex esse magni perspiciatis?</p>
+    </div>
+    </div>
+  )
+}
+
+export default HeroArea
